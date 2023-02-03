cdf7e5743e5c4685ed0c2674944c508e7e24cd63
Lightier <124357866+Lightier@users.noreply.github.com>
2023-02-03 14:41:28 +0300

    İntOlarakDeğerBelirleyipEkranaYazanKod
    Hüseyin

---
+++ İntOlarakDeğerBelirleyipEkranaYazanKod.c
@@ -0,0 +1,12 @@
+namespace GitHubProje
+{
+    class İntOlarakDeğerBelirleyipEkranaYazanKod
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
