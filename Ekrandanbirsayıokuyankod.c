daab714b6e6e52f16bd54d14cfb65043c7e04571
Lightier <124357866+Lightier@users.noreply.github.com>
2023-02-03 14:47:44 +0300

    c# dilinde ekrandan bir sayı okuyan kod
    Hüseyin

---
+++ Ekrandanbirsayıokuyankod.c
@@ -0,0 +1,15 @@
+namespace GitHubProje
+{
+    class Ekrandanbirsayıokuyankod
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            //ToInt... sayı değeri girilecek sayıya göre değişkenlşik gösterebilir.//
+            a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.ReadLine(); //ReadLine kullanılma sebebi ugyulamanın kapanmaması için//
+
+        }
+    }
+}
